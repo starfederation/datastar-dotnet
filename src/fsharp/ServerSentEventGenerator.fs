ea6627085d7c1255f4891dfb5f1a74d3275f2c49
Ben Croker <57572400+bencroker@users.noreply.github.com>
2026-03-27 11:50:45 -0600

    Handle DELETE method in `ReadSignals`

--- src/fsharp/ServerSentEventGenerator.fs
+++ src/fsharp/ServerSentEventGenerator.fs
@@ -134,7 +134,8 @@ type ServerSentEventGenerator(httpContextAccessor:IHttpContextAccessor) =
 
     static member GetSignalsStream(httpRequest:HttpRequest) =
         match httpRequest.Method with
-        | System.Net.WebRequestMethods.Http.Get ->
+        | System.Net.WebRequestMethods.Http.Get
+        | System.Net.WebRequestMethods.Http.Delete ->
             match httpRequest.Query.TryGetValue(Consts.DatastarKey) with
             | true, stringValues when stringValues.Count > 0 -> (new MemoryStream(Encoding.UTF8.GetBytes(stringValues[0])) :> Stream)
             | _ -> Stream.Null
@@ -143,7 +144,8 @@ type ServerSentEventGenerator(httpContextAccessor:IHttpContextAccessor) =
     static member ReadSignalsAsync(httpRequest:HttpRequest, cancellationToken:CancellationToken) =
         backgroundTask {
             match httpRequest.Method with
-            | System.Net.WebRequestMethods.Http.Get ->
+            | System.Net.WebRequestMethods.Http.Get
+            | System.Net.WebRequestMethods.Http.Delete ->
                 match httpRequest.Query.TryGetValue(Consts.DatastarKey) with
                 | true, stringValues when stringValues.Count > 0 -> return stringValues[0]
                 | _ -> return Signals.empty
@@ -159,7 +161,8 @@ type ServerSentEventGenerator(httpContextAccessor:IHttpContextAccessor) =
         task {
             try
                 match httpRequest.Method with
-                | System.Net.WebRequestMethods.Http.Get ->
+                | System.Net.WebRequestMethods.Http.Get
+                | System.Net.WebRequestMethods.Http.Delete ->
                     match httpRequest.Query.TryGetValue(Consts.DatastarKey) with
                     | true, stringValues when stringValues.Count > 0 ->
                         return ValueSome (JsonSerializer.Deserialize<'T>(stringValues[0], jsonSerializerOptions))
